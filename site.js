91444165d07705d1e6df15727e046a1cab7156fc
Leonardo Couto <leoctescossia11@gmail.com>
2022-10-17 20:14:08 -0300

    Primeiro site

---
+++ site.js
@@ -0,0 +1,19 @@
+function entrar(){
+    var entrou = document.getElementsByClassName('entrar');
+    window.location = "login.html"
+
+}
+function cadastrar(){
+    var cadastro = document.getElementsByClassName('cad');
+    window.location = "cadastro.html"
+
+}
+function login(){
+    window.location = "https://www.youtube.com/shorts/6gOFUOPl-Uo"
+}
+
+function botoes(){
+    var botoes = document.getElementsByClassName('botao');
+
+    botoes.innerHTML = ''
+}
